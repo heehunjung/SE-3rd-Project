7711d85701b9c6398a8be2595797db8ec7baaea9
heehun <slrkqk2@naver.com>
2024-10-03 21:14:17 +0900

    feat: filter 추가

---
+++ src/main/java/com/seProject/stockTrading/filter/JsonUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.seProject.stockTrading.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+
+public class JsonUsernamePasswordAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private static final String DEFAULT_LOGIN_URL = "/login";
+    private static final String HTTP_METHOD = "POST";
+    private static final String CONTENT_TYPE = "application/json";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+
+    private final ObjectMapper mapper;
+
+    private static final AntPathRequestMatcher DEFAULT_LOGIN_PATH_REQUEST_MATCHER =
+            new AntPathRequestMatcher(DEFAULT_LOGIN_URL, HTTP_METHOD);
+
+    public JsonUsernamePasswordAuthenticationFilter(ObjectMapper mapper) {
+        super(DEFAULT_LOGIN_PATH_REQUEST_MATCHER);
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        if (request.getContentType() == null || !request.getContentType().equals(CONTENT_TYPE)) {
+            throw new AuthenticationServiceException("Authentication Content-Type not supported: " + request.getContentType());
+        } // 좆버그
+
+        String messageBody = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+
+        Map<String,String> usernamePasswordMap = mapper.readValue(messageBody, Map.class);
+
+        String username = usernamePasswordMap.get(USERNAME);
+        String password = usernamePasswordMap.get(PASSWORD);
+
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+}
