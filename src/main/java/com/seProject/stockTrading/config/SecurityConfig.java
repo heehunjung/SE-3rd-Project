8bc64c099aca6f44028d7e695e1be66f12a52709
heehun <slrkqk2@naver.com>
2024-10-03 20:39:58 +0900

    feat: PasswordEncoder 추가

--- src/main/java/com/seProject/stockTrading/config/SecurityConfig.java
+++ src/main/java/com/seProject/stockTrading/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,25 +23,15 @@ public class SecurityConfig {
     private final MemberService memberService;
     private final ObjectMapper objectMapper;
 
-//    @Bean
-//    public WebSecurityCustomizer configure() {
-//        return (web) -> web.ignoring()
-//                .requestMatchers(toH2Console())
-//                .requestMatchers("/static/**")
-//    }
-    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http	.csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/signup", "/", "/login").permitAll()
+                        .requestMatchers("/", "/login").permitAll()
                         .anyRequest().authenticated())
-                // 폼 로그인은 현재 사용하지 않음
-//				.formLogin(formLogin -> formLogin
-//						.loginPage("/login")
-//						.defaultSuccessUrl("/home"))
                 .logout((logout) -> logout
                         .logoutSuccessUrl("/login")
                         .invalidateHttpSession(true))
@@ -50,7 +42,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public static PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
