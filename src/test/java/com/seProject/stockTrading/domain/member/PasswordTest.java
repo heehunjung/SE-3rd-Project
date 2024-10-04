d12467e54fbb70bfdac17f6e321dac47c9f894fe
heehun <slrkqk2@naver.com>
2024-10-04 12:40:04 +0900

    test: pw test

--- src/test/java/com/seProject/stockTrading/domain/member/PasswordTest.java
+++ src/test/java/com/seProject/stockTrading/domain/member/PasswordTest.java
@@ -29,6 +29,32 @@ public class PasswordTest {
         assertThat(encodedPassword).contains("{bcrypt}");
         assertThat(encodedPassword).isNotEqualTo(password);
     }
+
+    @Test
+    void 패스워드_랜덤_암호화() throws Exception {
+        // Config에 memberService
+        ApplicationContext ac = new AnnotationConfigApplicationContext(SecurityConfig.class,TestConfig.class);
+        PasswordEncoder encoder = ac.getBean(PasswordEncoder.class);
+
+        String password = "password";
+
+        String encodePassword = encoder.encode(password);
+        String encodePassword2 = encoder.encode(password);
+
+        assertThat(encodePassword).isNotEqualTo(encodePassword2);
+    }
+
+    @Test
+    void 패스워드_매치() throws Exception {
+        // Config에 memberService
+        ApplicationContext ac = new AnnotationConfigApplicationContext(SecurityConfig.class,TestConfig.class);
+        PasswordEncoder encoder = ac.getBean(PasswordEncoder.class);
+
+        String password = "password";
+        String encodePassword = encoder.encode(password);
+        assertThat(encoder.matches(password, encodePassword)).isTrue();
+
+    }
     // memberService의 구현체를 대충 넣어줄거임
     // 근데 결론적으론 분리하는게 나은가 원래 이런식으로 하나 모르겟네
     static class TestConfig {
